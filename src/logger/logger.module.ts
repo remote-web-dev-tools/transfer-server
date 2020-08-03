864af90b82cb346f5ec3da000160849bf7c7667f
lleohao <lleohao@hotmail.com>
2020-08-03 14:51:43 +0800

    refactor(service): import ResponseService

--- src/logger/logger.module.ts
+++ src/logger/logger.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { LoggerController } from './logger.controller';
 import { LoggerService } from './logger.service';
 import { ScheduleModule } from '@nestjs/schedule';
+import { ResponseService } from '../common/services/response.service';
 
 @Module({
   controllers: [LoggerController],
-  providers: [LoggerService],
+  providers: [LoggerService, ResponseService],
   imports: [ScheduleModule.forRoot()],
 })
 export class LoggerModule {}
