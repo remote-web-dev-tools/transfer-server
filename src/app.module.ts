d7375ebfd6eee9490e6afabb9b95ffa48532cb26
lleohao <lleohao@hotmail.com>
2020-09-07 19:47:43 +0800

    feat: add @nestjs/serve-static

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
 import { AppService } from './app.service';
 import { LoggerModule } from './logger/logger.module';
+import { join } from 'path';
 
 @Module({
-  imports: [LoggerModule],
+  imports: [
+    LoggerModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '../www'),
+      exclude: ['/api*'],
+    }),
+  ],
+
   providers: [AppService],
 })
 export class AppModule {}
