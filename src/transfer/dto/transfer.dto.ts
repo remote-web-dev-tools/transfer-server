edf39b1950b1453696af6f39a9bd857bf0d6dddf
lleohao <lleohao@hotmail.com>
2020-12-27 11:50:14 +0800

    feat: TransferDto add validator

--- src/transfer/dto/transfer.dto.ts
+++ src/transfer/dto/transfer.dto.ts
@@ -2,14 +2,19 @@ import { ApiProperty } from '@nestjs/swagger';
 
 import { TransferData } from '../interfaces/transfer-data.interface';
 import { ClientId, ServerId } from '../interfaces/transfer-id.interface';
+import { IsArray, IsOptional, IsString } from 'class-validator';
 
 export class TransferDto implements TransferData {
   @ApiProperty()
+  @IsArray()
   data: any[];
 
   @ApiProperty()
+  @IsString()
   serverId: ServerId;
 
   @ApiProperty({ required: false })
+  @IsString()
+  @IsOptional()
   clientId?: ClientId;
 }
