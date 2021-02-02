d080c71aab2863ee4abf3dc90c0b0c5646fa089c
lleohao <lleohao@hotmail.com>
2021-02-02 20:50:20 +0800

    feat: add request validate

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,10 @@
 import { Module } from '@nestjs/common';
-// import { ServeStaticModule } from '@nestjs/serve-static';
-// import { join } from 'path';
 
 import { AppService } from './app.service';
 import { TransferModule } from './transfer/transfer.module';
 
 @Module({
-  imports: [
-    // ServeStaticModule.forRoot({
-    //   rootPath: join(__dirname, '../www'),
-    //   exclude: ['/api*'],
-    // }),
-    TransferModule,
-  ],
+  imports: [TransferModule],
 
   providers: [AppService],
 
